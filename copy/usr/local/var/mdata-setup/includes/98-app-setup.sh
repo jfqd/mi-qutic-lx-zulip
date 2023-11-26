d22c1b8142892e56fe7feb578fb126bf9457ab2a
jfqd <jfqd@blun.org>
2023-11-26 21:47:25 +0100

    fix chrony service

--- copy/usr/local/var/mdata-setup/includes/98-app-setup.sh
+++ copy/usr/local/var/mdata-setup/includes/98-app-setup.sh
@@ -1,13 +1,44 @@
+#!/usr/bin/bash
 cd /root
 
-# EMAIL=$(/native/usr/sbin/mdata-get zulip_admin_email)
-# HOST=$(/native/usr/sbin/mdata-get zulip_external_host)
+export TMPDIR=/var/tmp
 
-# ./zulip-server-*/scripts/setup/install --self-signed-cert --email="${EMAIL}" --hostname="${HOST}"
+EMAIL=$(/native/usr/sbin/mdata-get zulip_admin_email)
+HOST=$(/native/usr/sbin/mdata-get zulip_external_host)
 
+# get it installed
+for ((i=1;i<=6;i++)); do
+  echo "* install zulip, running loop: ${i}"
+  /root/zulip-server-*/scripts/setup/install --self-signed-cert --email="${EMAIL}" --hostname="${HOST}"
+  if [ $? -eq 0 ]; then
+    break
+  fi
+done
+
+echo "* fix chrony service"
+sed -i \
+    -e "s|PIDFile=/run/chrony/chronyd.pid|#PIDFile=/run/chrony/chronyd.pid|" \
+    /etc/systemd/system/chrony.service
+systemctl daemon-reload
+systemctl start chrony
+
+echo "* setup mailer"
 # https://zulip.readthedocs.io/en/stable/production/email.html
-# /etc/zulip/settings.py
-# /etc/zulip/zulip-secrets.conf
+if /native/usr/sbin/mdata-get mail_smarthost 1>/dev/null 2>&1; then
+  echo "* Setup mail"
+  MAIL_UID=$(/native/usr/sbin/mdata-get mail_auth_user)
+  MAIL_PWD=$(/native/usr/sbin/mdata-get mail_auth_pass)
+  MAIL_HOST=$(/native/usr/sbin/mdata-get mail_smarthost)
+  sed -i \
+      -e "s|# EMAIL_HOST = \"smtp.example.com\"|EMAIL_HOST = \"${MAIL_HOST}\"|" \
+      -e "s|# EMAIL_HOST_USER = \"\"|EMAIL_HOST_USER = \"${MAIL_UID}\"|" \
+      -e "s|# EMAIL_USE_TLS = True|EMAIL_USE_TLS = True|" \
+      -e "s|# EMAIL_PORT = 587|EMAIL_PORT = 587|" \
+      /etc/zulip/settings.py
+  echo "email_password = \"${MAIL_PWD}\"" >> /etc/zulip/zulip-secrets.conf
+fi
+chmod 0640 /etc/zulip/settings.py
+
 
 # [loadbalancer]
 # # Use the IP addresses you determined above, separated by commas.
